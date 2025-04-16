1a1fa3d9da6491ab822af664ccefa5c5ccff8db7
BibekRaja <47000134+BibekRaja@users.noreply.github.com>
2025-04-16 17:48:35 +0545

    connecting database in php and storing data using php

---
+++ connect-database/includes/formhandler.inc.php
@@ -0,0 +1,49 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+    $email = $_POST["email"];
+
+    try {
+
+        require_once "dbh.inc.php";
+        // storing data using not name parameter
+        /* 
+        $query = "INSERT INTO users (username, pwd, email) VALUES 
+         (?, ?, ?);";
+
+         $stmt = $pdo->prepare($query);
+
+         $stmt->execute([$username, $pwd, $email]);
+         */
+
+        // storing data using name parameters
+
+        $query = "INSERT INTO users (username, pwd, email) VALUES 
+        (:username, :pwd, :email);";
+
+        $stmt = $pdo->prepare($query);
+
+        $stmt->bindParam(":username", $username);
+        $stmt->bindParam(":pwd", $pwd);
+        $stmt->bindParam(":email", $email);
+
+        $stmt->execute();
+
+
+        $pdo = null;
+        $stmt = null;
+
+        header("Location: ../index.php");
+
+        die();
+
+    } catch (PDOExeption $e) {
+        die("Query failed: " . e->getMessage());
+    }
+
+} else {
+    header("Location: ../index.php");
+}
